eadd08c9d07c66708a1c19cb0c2ad3187ba882b3
Yixin Zhang <yixin.zhang@arm.com>
2018-10-05 10:06:16 -0500

    add chromedriver and openchrome.py

---
+++ openchrome.py
@@ -0,0 +1,28 @@
+import time
+from selenium import webdriver
+
+# Main Page
+
+driver = webdriver.Chrome('/Users/yixzha01/Desktop/chromedriver') 
+driver.get('https://cat.deploymbed.com/main#/');
+driver.fullscreen_window()
+time.sleep(5)
+driver.save_screenshot('main.png')
+
+# Floor Page
+
+driver.find_element_by_xpath("//*[@id='5b2bd35272782e62cded0465']").click()
+driver.find_element_by_xpath("//*[@id='selFloor']/option[1]").click()
+time.sleep(5)
+driver.save_screenshot('floor.png')
+
+# Admin Page
+
+link = driver.find_element_by_xpath("//*[@id='settings']")
+print (link)
+link.click()
+time.sleep(5)
+
+driver.quit()
+
+
