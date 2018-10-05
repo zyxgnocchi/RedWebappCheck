ac64b40b0be77cbeedde756f6250a04298bfe431
Yixin Zhang <yixin.zhang@arm.com>
2018-10-05 12:17:30 -0500

    modify openchrome.py

--- openchrome.py
+++ openchrome.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 # Main Page
 
-driver = webdriver.Chrome('/builds/ws/RedWebappCheck/chromedriver') 
+driver = webdriver.Chrome() 
 driver.get('https://cat.deploymbed.com/main#/');
 driver.fullscreen_window()
 time.sleep(5)
